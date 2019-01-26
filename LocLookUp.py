31857001dec69c2f265af587610f56dcfb851836
Natasha <varghn1@mcmaster.ca>
2019-01-26 16:38:33 -0500

    shortened return

--- LocLookUp.py
+++ LocLookUp.py
@@ -1,8 +1,14 @@
 import urllib2
+import requests
 
 def read(latitude,longitude):
     url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key=AIzaSyBW5QZVpvnxWPcemecQBxhxZrjPjxB2bXU'.format(latitude,longitude)
     r = urllib2.urlopen(url)
-    print(r.read())
+    
+    json_data = requests.get(url).json()
+
+    place = json_data['results'][0]['formatted_address']
+
+    print(place)
 
 read(49.15563,-123.0172)
