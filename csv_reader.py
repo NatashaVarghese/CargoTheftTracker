20c057dbf016a59fbaaf904a69c2feebe8bdf8fc
Ian Currie <iancurrie@Ians-MacBook-Pro.local>
2019-01-26 16:35:54 -0500

    Update to csv_reader for columns

--- csv_reader.py
+++ csv_reader.py
@@ -4,13 +4,31 @@ def readMyFile(filename):
     dict = {}
     with open(filename) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
+        csvReader.next()    #for getting rid of header file
         for row in csvReader:
+
+            # key is the Device Serial
+            # value is [ID,Vin,MessageType,ReportType,MsgNum,TripState,ReceivedTime,
+            #           Latitude,Longitude,CollectionTimeStamp,ManufacturerSerial]
+
+
+
             key  = row[1]
-            val = [row[0]] + row[2:]
             if dict.has_key(key):            
-                dict[key].append(val)
+                dict[key][0].append(row[0])
+                dict[key][1].append(row[2])
+                dict[key][2].append(row[3])
+                dict[key][3].append(row[4])
+                dict[key][4].append(row[5])
+                dict[key][5].append(row[6])
+                dict[key][6].append(row[7])
+                dict[key][7].append(row[8])
+                dict[key][8].append(row[9])
+                dict[key][9].append(row[13])
+                dict[key][10].append(row[14])
             else:
-                dict[key] = [val]
+                dict[key] = [[row[0]],[row[2]],[row[3]],[row[4]],[row[5]],
+                [row[6]],[row[7]],[row[8]],[row[9]],[row[13]],[row[14]]]
             
     return dict
 
