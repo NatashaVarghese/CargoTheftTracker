1bb8c77df10b5cacb692c4324ce6c89e1843eae4
Natasha Varghese <natashavarghese920@gmail.com>
2019-01-27 10:19:54 -0500

    Delete getLatLong.py

--- getLatLong.py
+++
@@ -1,9 +0,0 @@
-import csv_reader 
-
-def latlong(dictionary,truckID):
-    for i in range(len(dictionary[truckID][7])):
-        print(dictionary[truckID][7][i])
-        print(dictionary[truckID][8][i])
-
-latlong(csv_reader.readMyFile('ITM_20190121.csv'),'1084067241') 
-
