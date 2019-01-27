6e79c989ee6db9375a9faecb2794eb910aa8f54f
Natasha <varghn1@mcmaster.ca>
2019-01-26 23:22:23 -0500

    changes to warnings

--- acceleration_warnings.py
+++ acceleration_warnings.py
@@ -1,10 +1,8 @@
 import csv_reader 
 
-def acceleration_warnings(dictionary,truckID,entry):
-    for i in range(len(dictionary[truckID][2])):
-        if dictionary[truckID][2][i]=="Acceleration" and dictionary[truckID][0][i]==entry:
-            return True
-            
+def acceleration_warnings(dictionary,truckID,index):
+    if dictionary[truckID][2][index]=="Acceleration":
+        return True
+    return False            
 
-print(acceleration_warnings(csv_reader.readMyFile('ITM_20190121.csv'),'1088214013','922454770'))
 
--- time_warnings.py
+++ time_warnings.py
@@ -1,15 +1,12 @@
 import csv_reader 
 import datetime 
 
-def time_warnings(dictionary,truckID,entry):
-    for i in range(len(dictionary[truckID][9])):
-        if dictionary[truckID][0][i]==entry:
-            result = dictionary[truckID][9][i].replace("-"," ").split()
-            date = datetime.date(int(result[0]),int(result[1]),int(result[2]))
-            if date.weekday()==0 or date.weekday()==4 or date.weekday()==5 or date.weekday()==6:
-                return True
-            return False
+def time_warnings(dictionary,truckID,index):
+    result = dictionary[truckID][9][index].replace("-"," ").split()
+    date = datetime.date(int(result[0]),int(result[1]),int(result[2]))
+    if date.weekday()==0 or date.weekday()==4 or date.weekday()==5 or date.weekday()==6:
+        return True
+    return False
         
     
-print(time_warnings(csv_reader.readMyFile('ITM_20190121.csv'),'1088217019','920007354'))
 
